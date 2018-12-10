c4b46ec21c3f814dd049deb19a7bb87aa0547b7b
rduncan <rduncan@phpatlanta.com>
2018-12-09 23:22:09 -0500

    fixes html-routes bug

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = function(app) {
     
     app.get('/points', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/html/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
     app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/html/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
